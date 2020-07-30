b7753bdd84695772f1e31a468e18b77dea475f8d
FirzanR <46702357+FirzanRisnanda@users.noreply.github.com>
2020-07-30 13:32:46 +0700

    update 30 juli

--- App.js
+++ App.js
@@ -132,7 +132,6 @@ function App() {
           options={{
             tabBarLabel:'',
             tabBarIcon: () => (
-              
                 <MaterialCommunityIcons  
                 name="home-outline" 
                 color='#517fa4' 
